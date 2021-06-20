e98614701eeaa3bafc139765da1cd4e55e0976bf
strongtino <strongtino.business@gmail.com>
2021-06-20 14:11:43 +0200

    Added StringUtil::sliceList and isInteger

--- src/main/java/dev/strongtino/soteria/util/StringUtil.java
+++ src/main/java/dev/strongtino/soteria/util/StringUtil.java
@@ -1,6 +1,8 @@
 package dev.strongtino.soteria.util;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 public class StringUtil {
 
@@ -17,5 +19,23 @@ public class StringUtil {
         int stringLength = builder.toString().length();
 
         return builder.substring(0, stringLength - (stringLength > 2 ? 2 : 0));
+    }
+
+    public static <T> List<List<T>> sliceList(List<T> list, int size) {
+        List<List<T>> parts = new ArrayList<>();
+
+        for (int i = 0; i < list.size(); i += size) {
+            parts.add(new ArrayList<>(list.subList(i, Math.min(list.size(), i + size))));
+        }
+        return parts;
+    }
+
+    public static boolean isInteger(String text) {
+        try {
+            Integer.parseInt(text);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
     }
 }
