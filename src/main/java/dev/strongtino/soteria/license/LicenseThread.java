3e716d34d50fe1cc8102cdbca11ba2803da4e102
strongtino <strongtino.business@gmail.com>
2021-06-20 15:05:36 +0200

    Fixed concurrent modification exception while revoking licenses

--- src/main/java/dev/strongtino/soteria/license/LicenseThread.java
+++ src/main/java/dev/strongtino/soteria/license/LicenseThread.java
@@ -2,15 +2,25 @@ package dev.strongtino.soteria.license;
 
 import dev.strongtino.soteria.Soteria;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class LicenseThread extends Thread {
 
     @Override
     public void run() {
         while (true) {
+            List<String> toRemove = new ArrayList<>();
+
             Soteria.INSTANCE.getLicenseService().getLicenses()
                     .stream()
                     .filter(license -> license.isExpired() && license.isActive())
-                    .forEach(license -> Soteria.INSTANCE.getLicenseService().revokeLicense(license));
+                    .forEach(license -> {
+                        Soteria.INSTANCE.getLicenseService().revokeLicense(license);
+                        toRemove.add(license.getKey());
+                    });
+
+            toRemove.forEach(key -> Soteria.INSTANCE.getLicenseService().removeLicenseFromMap(key));
             try {
                 Thread.sleep(1000);
             } catch (InterruptedException e) {
